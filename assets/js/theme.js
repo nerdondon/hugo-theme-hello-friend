59d1a88b35f5c8c1a799b76cd6650a629c4e806a
nerdondon <willdon@nerdondon.com>
2021-10-08 04:51:28 +0000

    Format js with prettier

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,18 +1,17 @@
 // Toggle theme
-
-const getTheme = window.localStorage && window.localStorage.getItem("theme");
-const themeToggle = document.querySelector(".theme-toggle");
-const isDark = getTheme === "dark";
+const getTheme = window.localStorage && window.localStorage.getItem('theme');
+const themeToggle = document.querySelector('.theme-toggle');
+const isDark = getTheme === 'dark';
 
 if (getTheme !== null) {
-  document.body.classList.toggle("dark-theme", isDark);
+  document.body.classList.toggle('dark-theme', isDark);
 }
 
-themeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme");
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme');
   window.localStorage &&
     window.localStorage.setItem(
-      "theme",
-      document.body.classList.contains("dark-theme") ? "dark" : "light",
+      'theme',
+      document.body.classList.contains('dark-theme') ? 'dark' : 'light'
     );
 });
